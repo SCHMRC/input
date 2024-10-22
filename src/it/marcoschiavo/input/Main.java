7a99ec6f1e0ffb0bf18889daa57556fb1c5318b2
SCHMRC <schiavo.marco05@gmail.com>
2024-10-22 10:45:05 +0200

    inserito uno step precedente

--- src/it/marcoschiavo/input/Main.java
+++ src/it/marcoschiavo/input/Main.java
@@ -6,10 +6,23 @@ import java.io.IOException;
 import java.util.Scanner;
 public class Main {
 
-//	public static void main(String args[]) {
-//		InputStreamReader stream = new InputStreamReader(System.in);
-//		BufferedReader keybord = new BufferedReader(stream);
-//		
+	public static void main(String args[]) {
+		InputStreamReader stream = new InputStreamReader(System.in);
+		BufferedReader keybord = new BufferedReader(stream);
+		byte x = 0;
+		try {
+			System.out.println("Inserisci il valore numero 1");
+			String uno = keybord.readLine();
+			System.out.println("Inserisci il valore numero 2");
+			String due = keybord.readLine();
+			System.out.println("Inserisci il valore numero 3");
+			String tre = keybord.readLine();
+			
+		}catch(Exception e) {
+			
+		}
+		
+		
 //		try {
 //			int somma = 0 ;
 //			final int COSTANTE = 3 ;
@@ -24,14 +37,14 @@ public class Main {
 //		}catch(IOException x) {
 //			System.err.println(x.getMessage());
 //		}
-//		
-//	}
+		
+	}
 	
 	//variante con la classe scanner
-	public static void main(String args[]) {
-		String nome;
-		Scanner scanner = new Scanner(System.in);
-		nome = scanner.next();
-	}
+//	public static void main(String args[]) {
+//		String nome;
+//		Scanner scanner = new Scanner(System.in);
+//		nome = scanner.next();
+//	}
 
 }
